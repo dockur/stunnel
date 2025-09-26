652759d57616d0ace75129e9cebe7f785cb6ecba
Kroese <kroese@users.noreply.github.com>
2025-09-26 02:25:35 +0200

    feat: Initial commit (#1)

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,27 @@
-ARG ALPINE_VERSION=edge
-ARG USER=stunnel
+# syntax=docker/dockerfile:1
 
-FROM alpine:${ALPINE_VERSION} as builder
-ENV TZ="UTC" \
-  PUID="1000" \
-  PGID="1000"
+FROM alpine:edge
 
-ARG USER
-RUN apk --update --no-cache add stunnel openssl
-RUN rm -rf /etc/stunnel/stunnel.conf /tmp/* /var/cache/apk/*
-RUN stunnel -version
+RUN set -eu && \
+    apk --no-cache add \
+    tini \
+    bash \
+    openssl \
+    stunnel && \
+    rm -rf /etc/stunnel/stunnel.conf && \
+    rm -rf /tmp/* /var/cache/apk/*
 
-COPY rootfs /
-RUN chmod +x /init
-
+COPY --chmod=755 stunnel.sh /usr/bin/stunnel.sh
 RUN ln -sf /dev/stdout /var/log/stunnel.log
 
+ENV TZ="UTC"
+ENV CLIENT: "no"
+ENV LISTEN_PORT: "853"
+ENV CONNECT_PORT: "53"
+ENV CONNECT_HOST: "10.0.0.1"
+      
 VOLUME [ "/etc/stunnel" ]
 
-ENTRYPOINT [ "/init" ]
-
 HEALTHCHECK --interval=10s --timeout=5s --start-period=5s CMD /usr/local/bin/healthcheck
+
+ENTRYPOINT ["/sbin/tini", "--", "/usr/bin/stunnel.sh"]
---
+++ stunnel.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# shellcheck disable=SC2209
+echo=echo
+for cmd in echo /bin/echo; do
+	$cmd >/dev/null 2>&1 || continue
+	if ! $cmd -e "" | grep -qE '^-e'; then
+		echo=$cmd
+		break
+	fi
+done
+
+cli=$($echo -e "\033[")
+norm="${cli}0m"
+bold="${cli}1;37m"
+# red="${cli}1;31m"
+yellow="${cli}1;33m"
+green="${cli}1;32m"
+
+# Variables
+TZ=${TZ:-UTC}
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+CLIENT=${CLIENT:-no}
+LISTEN_HOST=${LISTEN_HOST:-0.0.0.0}
+LISTEN_PORT=${LISTEN_PORT:-853}
+CONNECT_HOST=${CONNECT_HOST:-10.0.0.1}
+CONNECT_PORT=${CONNECT_PORT:-53}
+HEALTHCHECK=${HEALTHCHECK:-127.0.0.1}
+
+echo -e "\n${bold}Starting Stunnel for Docker...${norm} ($LISTEN_HOST:$LISTEN_PORT => $CONNECT_HOST:$CONNECT_PORT)\n"
+
+# Timezone
+echo "  ${norm}[${green}+${norm}] Setting timezone to ${green}${TZ}${norm}"
+ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime
+echo "${TZ}" > /etc/timezone
+
+# Check certificate & key
+if [ -f /etc/stunnel/stunnel.pem ] && [ -s /etc/stunnel/stunnel.pem ]; then
+  chmod 640 /etc/stunnel/stunnel.pem
+  rm -f  /etc/stunnel/stunnel.crt
+  rm -f  /etc/stunnel/stunnel.key
+  echo -e "  ${norm}[${yellow}+${norm}] Converting certificate format..."
+  openssl pkey -in  /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.key
+  openssl x509 -outform PEM -in  /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.crt
+fi
+
+if [ ! -f /etc/stunnel/stunnel.crt ] || [ ! -f /etc/stunnel/stunnel.key ]; then
+  echo -e "  ${norm}[${yellow}+${norm}] Generating self-signed certificate..."
+  openssl ecparam -genkey -name prime256v1 -out /etc/stunnel/stunnel.key
+  openssl req -new -x509 -sha512 -nodes -days 3652 \
+    -subj "/C=FR/ST=SSL/L=SSL/O=SSL/CN=SSL" \
+    -key /etc/stunnel/stunnel.key -out /etc/stunnel/stunnel.crt
+fi
+
+chmod 640 /etc/stunnel/stunnel.key
+
+# Check configuration
+if [ ! -f /etc/stunnel/stunnel.conf ]; then
+	echo "  ${norm}[${green}+${norm}] Setting up stunnel configuration.."
+	cat > /etc/stunnel/stunnel.conf << EOF
+foreground = yes
+output = /var/log/stunnel.log
+
+[Client]
+client = $CLIENT
+accept = $LISTEN_HOST:$LISTEN_PORT
+connect = $CONNECT_HOST:$CONNECT_PORT
+
+delay = no
+renegotiation = no
+sslVersionMin = TLSv1.2
+sslVersionMax = TLSv1.3
+cert=/etc/stunnel/stunnel.crt
+key=/etc/stunnel/stunnel.key
+
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+socket = r:TCP_KEEPCNT=4
+socket = r:TCP_KEEPIDLE=40
+socket = r:TCP_KEEPINTVL=5
+socket = r:SO_KEEPALIVE=1
+EOF
+fi
+
+# Fix permissions
+echo -e "  ${norm}[${green}+${norm}] Fixing permissions..${norm}\n"
+chown "${PUID}:${PGID}" /proc/self/fd/1 /proc/self/fd/2
+chown -R "${PUID}:${PGID}" /etc/stunnel /var/log/stunnel.log
+
+if [ -n "${PGID}" ] && [ -n "${PUID}" ]; then
+  sed -i -e "s/^stunnel:\([^:]*\):[0-9]*/stunnel:\1:${PGID}/" /etc/group
+  sed -i -e "s/^stunnel:\([^:]*\):\([0-9]*\):[0-9]*/stunnel:\1:\2:${PGID}/" /etc/passwd
+  sed -i -e "s/^stunnel:\([^:]*\):[0-9]*:\([0-9]*\)/stunnel:\1:${PUID}:\2/" /etc/p asswd
+fi
+
+# Healthcheck
+cat > /usr/local/bin/healthcheck << EOF
+#!/usr/bin/env sh
+set -e
+nc -w 1 -z $HEALTHCHECK $LISTEN_PORT
+EOF
+chmod +x /usr/local/bin/healthcheck
+
+# Init
+exec /usr/bin/stunnel
